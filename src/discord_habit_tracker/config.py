f65d6e7e150e2902a1e487d307883d33845b3527
Jonathan Kenneth Goh <77948882+jonkengoh@users.noreply.github.com>
2026-08-15 23:16:58 +0800

    feat: add class Config
    Add class Config for app config and discord token

--- src/discord_habit_tracker/config.py
+++ src/discord_habit_tracker/config.py
@@ -9,3 +9,15 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+
+class Config:
+    """Stores application configuration."""
+
+    def __init__(self):
+        bot_token = os.getenv("DISCORD_TOKEN")
+
+        if not bot_token:
+            raise ValueError("DISCORD_TOKEN is not configured.")
+
+        self.bot_token = bot_token
